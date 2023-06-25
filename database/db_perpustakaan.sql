4023af50f11d2332942668c99645096f24a30b1b
muhammad wiman <wiman1235@gmail.com>
2023-06-25 10:06:13 +0700

    database

---
+++ database/db_perpustakaan.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 25, 2023 at 04:57 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `list_buku`
+--
+
+CREATE TABLE `list_buku` (
+  `kode_buku` varchar(15) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `penerbit` varchar(25) NOT NULL,
+  `kategori` varchar(25) NOT NULL,
+  `pengarang` varchar(25) NOT NULL,
+  `tahun_terbit` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `list_buku`
+--
+
+INSERT INTO `list_buku` (`kode_buku`, `judul`, `penerbit`, `kategori`, `pengarang`, `tahun_terbit`) VALUES
+('A001', 'Malin Kundang', 'CV. Pustaka Setia, 2004.', 'Cerita Rakyat', 'Rini Kurniasih', '2006-12-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_pinjam` int(3) NOT NULL,
+  `kode_buku` varchar(15) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `tanggal_peminjaman` date NOT NULL,
+  `tanggal_pengembalian` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id` int(3) NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `denda` decimal(10,0) NOT NULL,
+  `id_pinjam` int(3) NOT NULL,
+  `username` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrasi`
+--
+
+CREATE TABLE `registrasi` (
+  `id` int(3) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `alamat` text NOT NULL,
+  `pekerjaan` varchar(25) NOT NULL,
+  `umur` int(3) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `registrasi`
+--
+
+INSERT INTO `registrasi` (`id`, `nama`, `alamat`, `pekerjaan`, `umur`, `username`, `password`) VALUES
+(1, 'wiman', 'rajabasa', 'pelajar', 21, 'wiman123', 'wiman12345'),
+(2, 'Muhammad Wiman', 'Labuhan Ratu', 'Mahasiswa', 21, 'wiman321', 'wiman123!');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `list_buku`
+--
+ALTER TABLE `list_buku`
+  ADD PRIMARY KEY (`kode_buku`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_pinjam`),
+  ADD KEY `kode_buku` (`kode_buku`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pinjam` (`id_pinjam`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `registrasi`
+--
+ALTER TABLE `registrasi`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_pinjam` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registrasi`
+--
+ALTER TABLE `registrasi`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`username`) REFERENCES `registrasi` (`username`),
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`kode_buku`) REFERENCES `list_buku` (`kode_buku`);
+
+--
+-- Constraints for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD CONSTRAINT `pengembalian_ibfk_1` FOREIGN KEY (`username`) REFERENCES `registrasi` (`username`),
+  ADD CONSTRAINT `pengembalian_ibfk_2` FOREIGN KEY (`id_pinjam`) REFERENCES `peminjaman` (`id_pinjam`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
